e5e6ee7ad6dca0e4587b6e0862cc23d9ebc29ad3
stefanthorpe <stefan.thorpe@gmail.com>
2017-05-24 15:29:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World from Caylent.</p>'; ?> 
-  <?php echo '<p>A light weight test php app.</p>'; ?> 
+ <?php echo '<p>Hello World, Caylent.</p>'; ?> 
  </body>
 </html>
